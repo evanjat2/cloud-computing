33700353ff4efd0ee37cd70316a4120746c90efe
alfathlathif <alfathns1932@gmail.com>
2023-12-05 11:56:34 +0700

    change route get spesific article

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,7 @@ const authHeader = require("../middleware/auth-header")
 
 let routes = (app) => {
   router.get("/article", articleController.getAllArticle);
-  router.get("/article/:id", articleController.getSpecificArticle);
+  router.get("/article/detail", articleController.getSpecificArticle);
 
   router.post("/scan-result/upload", mlOuputController.upload);
   router.get("/scan-result/list", mlOuputController.getListFiles);
