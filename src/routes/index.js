4322b73dd466da8afd7260907654109caa559e04
alfathlathif <alfathns1932@gmail.com>
2023-12-05 11:14:20 +0700

    add route store and get image scan

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ const articleController = require("../controller/article-controller");
 const mlOuputController = require("../controller/ml-output-controller");
 const authController = require("../controller/authController");
 const foodEmission = require("../controller/food-emission-controller");
+const storeImage = require("../controller/store-image-controller");
+const authHeader = require("../middleware/auth-header")
 
 let routes = (app) => {
   router.get("/article", articleController.getAllArticle);
@@ -18,6 +20,8 @@ let routes = (app) => {
 
   router.get("/emission/:name", foodEmission.getEmission);
 
+  router.patch("/store-image", authHeader.auth, storeImage.storeImage)
+  router.patch("/get-image", authHeader.auth, storeImage.getImage)
 
   app.use(router);
 };
