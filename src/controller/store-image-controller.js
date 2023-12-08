61cee47b625c71497abb20810438b783b839f44f
alfathlathif <alfathns1932@gmail.com>
2023-12-08 16:00:27 +0700

    add foodEmission variable to be store in firestore

--- src/controller/store-image-controller.js
+++ src/controller/store-image-controller.js
@@ -2,17 +2,43 @@ const { db } = require("../db/firestore");
 
 const storeImage = async (req, res) => {
   try {
+    //Get image url and food name from request body
     const { imageURL, foodName } = req.body;
+    let emission = ""
+
+    //Verified user by token
     const userId = req.user.userId;
 
+    //Access collection
+    const collectionRef = db.collection("food-emission");
+
+    //Query food emission from db food emission
+    const snapshot = await collectionRef.where("foodName", "==", foodName).get();
+
+    //Handle if foodName is not found
+    if (snapshot.empty) {
+      console.log("No matching documents.");
+      return;
+    }
+
+    //Access data and send it
+    snapshot.forEach((doc) => {
+      const data = doc.data();
+      emission = data.value
+    });
+
+
+    //Stored all information to firebase
     const docRef = db.collection("saved-image").doc();
     await docRef.set({
       userId: userId,
       imageURL: imageURL,
       foodName: foodName,
+      foodEmission: emission
     });
 
-    const data = { userId, imageURL, foodName };
+    //Set up variabel for response
+    const data = { userId, imageURL, foodName, foodEmission: emission };
 
     res.status(200).json({ data, message: "sent successful" });
   } catch (error) {
@@ -21,7 +47,7 @@ const storeImage = async (req, res) => {
   }
 };
 
-const getImage = async (req, res) => {
+const getFoodInfo = async (req, res) => {
   try {
     const userId = req.user.userId;
 
@@ -47,5 +73,5 @@ const getImage = async (req, res) => {
 
 module.exports = {
   storeImage,
-  getImage,
+  getFoodInfo,
 };
