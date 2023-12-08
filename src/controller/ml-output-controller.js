3edaed38cd4d096dc7bd1f0b3cb0b2669cc04b62
alfathlathif <alfathns1932@gmail.com>
2023-12-08 15:59:03 +0700

    add comment

--- src/controller/ml-output-controller.js
+++ src/controller/ml-output-controller.js
@@ -7,31 +7,42 @@ const bucket = storage.bucket("ml-ouput-eco-scan-bucket");
 
 const upload = async (req, res) => {
   try {
+    //Get image file from user and compress it with processFile
     await processFile(req, res);
 
+    //Check image is already upload or not
     if (!req.file) {
       return res.status(400).send({ message: "Please upload a file!" });
     }
 
+    //-------------------------------Store Image To Cloud Storage-------------------------------//
+    //Get image fie name
     const blob = bucket.file(req.file.originalname);
+
+    //Open stream processing
     const blobStream = blob.createWriteStream({
       resumable: false,
     });
 
+    //Make image file become local so can process it with pipe
     const localReadStream = new Readable();
     localReadStream.push(req.file.buffer);
     localReadStream.push(null); // Signal the end of the stream
 
+    //Start pipe to store image file on google storage
     localReadStream.pipe(blobStream)
+      //Error handler
       .on('error', (err) => {
         console.log(err);
         res.status(500).send({ message: err.message });
       })
+      //Finish handler
       .on('finish', async () => {
         const publicUrl = format(
           `https://storage.googleapis.com/${bucket.name}/${blob.name}`
         );
 
+        //Send image url
         try {
           await bucket.file(req.file.originalname).makePublic();
           res.status(200).send({
@@ -41,11 +52,11 @@ const upload = async (req, res) => {
         } catch (err) {
           console.log(err);
           res.status(500).send({
-            message: `Uploaded the file successfully: ${req.file.originalname}, but public access is denied!`,
-            url: publicUrl,
+            message: `Uploaded the file unsuccessfully:`,
           });
         }
       });
+    //-------------------------------Store Image To Cloud Storage-------------------------------//
   } catch (err) {
     console.log(err);
 
