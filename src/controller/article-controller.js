74d63f95f6d2639a1cbe16a4a3743c519b82cd6e
alfathlathif <alfathns1932@gmail.com>
2023-12-08 14:46:48 +0700

    give comment and change getSpecificArticle output

--- src/controller/article-controller.js
+++ src/controller/article-controller.js
@@ -1,16 +1,23 @@
+//Initiate access to firestore
 const { db } = require("../db/firestore");
 
+//Function to get all article from db
 const getAllArticle = async (req, res) => {
   try {
+    //Access collection
     const articleRef = db.collection("article-content");
+
+    //Get all article
     const response = await articleRef.get();
     let responseArr = [];
 
+    //Access all data and push it to array
     response.forEach((doc) => {
       responseArr.push(doc.data());
     });
 
-    res.send(responseArr);
+    //Send array and status code
+    res.status(200).send(responseArr);
   } catch (error) {
     console.log(error);
   }
@@ -18,24 +25,27 @@ const getAllArticle = async (req, res) => {
 
 const getSpecificArticle = async (req, res) => {
   try {
+    //Get title from request body
     const { title } = req.body;
 
+    //Access collection
     const articleRef = db.collection("article-content");
 
+    //Query db by title from request
     const snapshot = await articleRef.where("title", "==", title).get();
 
+    //Handle if not found title
     if (snapshot.empty) {
       console.log("No matching documents.");
       return;
     }
 
-    const dataArray = [];
+    //Access data and send it
     snapshot.forEach((doc) => {
       const data = doc.data();
-      dataArray.push({ id: doc.id, data });
+      res.status(200).json({id:doc.id, data});
     });
 
-    res.status(200).send(dataArray);
   } catch (error) {
     console.log(error);
   }
