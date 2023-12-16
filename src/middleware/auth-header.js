1bf4b073004059e8f8cd554c124dccc3ce363e7b
Alfath Nuurlathif <79137289+alfathlathif@users.noreply.github.com>
2023-12-16 22:32:20 +0700

    delete print token

--- src/middleware/auth-header.js
+++ src/middleware/auth-header.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 
 const auth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  console.log(authHeader);
   if (!authHeader || !authHeader.startsWith("Bearer")) {
     console.log("Authentication Invalid");
   }
