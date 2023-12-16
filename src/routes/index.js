478f582fe561499f037e66bd17570a91c72369c8
alfathlathif <alfathns1932@gmail.com>
2023-12-16 21:45:54 +0700

    delete some routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const articleController = require("../controller/article-controller");
 const mlOuputController = require("../controller/ml-output-controller");
 const authController = require("../controller/authController");
-const foodEmission = require("../controller/food-emission-controller");
-const storeImage = require("../controller/store-image-controller");
+const storeImage = require("../controller/store-model-output-controller");
 const authHeader = require("../middleware/auth-header")
 
 let routes = (app) => {
@@ -12,16 +11,14 @@ let routes = (app) => {
   router.get("/article/:id", articleController.getSpecificArticle);
 
   router.post("/scan-result/upload", mlOuputController.upload);
-  router.get("/scan-result/list", mlOuputController.getListFiles);
-  router.get("/scan-result/list/:name", mlOuputController.download);
+  // router.get("/scan-result/list", mlOuputController.getListFiles);
+  // router.get("/scan-result/list/:name", mlOuputController.download);
 
   router.post("/auth/signUp", authController.signUpUsers);
   router.post("/auth/login",authController.loginUsers);
 
-  router.get("/emission/:name", foodEmission.getEmission);
-
-  router.patch("/store-image", authHeader.auth, storeImage.storeImage)
-  router.patch("/get-food-info", authHeader.auth, storeImage.getFoodInfo)
+  router.patch("/store-result", authHeader.auth, storeImage.storeModelOutput)
+  router.patch("/get-result-info", authHeader.auth, storeImage.getOutputInfo)
 
   app.use(router);
 };
