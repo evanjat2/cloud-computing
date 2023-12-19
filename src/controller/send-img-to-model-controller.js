9f30184087662726148b75b9f7712b3ca9ff8b9c
alfathlathif <alfathns1932@gmail.com>
2023-12-19 16:01:13 +0700

    function for push image from javascript side to flask side

--- src/controller/send-img-to-model-controller.js
+++ src/controller/send-img-to-model-controller.js
@@ -1,66 +1,50 @@
 const axios = require("axios");
 
-const quota = require("../controller/quotaController");
+const quota = require("./quotaController");
 
-const sendImageToFlaskEndpoint = async (imageData) => {
+// Function for sending image_url to flask
+const sendImageUrlToFlask = async (image_url) => {
   try {
-    const flaskEndpoint = "https://predict-pbjv724rza-et.a.run.app/predict/";
+    const flaskEndpoint = process.env.MODEL_ENDPOINT;
 
-    // Send image
-    const response = await axios.post(
-      flaskEndpoint,
-      { image: imageData },
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      }
-    );
+    // Send image_url to flask
+    const response = await axios.post(flaskEndpoint, { image_url });
 
-    // Stored response endpoint
-    const flaskResponse = response.data;
+    // Get response
+    const modelResponse = response.data;
 
-    console.log(flaskResponse);
-
-    return flaskResponse;
+    return modelResponse;
   } catch (error) {
-    console.error("Error sending image to Flask endpoint:", error.message);
-    throw error;
+    console.error('Error sending image URL to Flask endpoint:', error.message);
+    throw new Error('Failed to send image URL to Flask endpoint');
   }
 };
 
+// Function to proceed all data
 const processData = async (req, res) => {
-  //Verified user by token
-  const userId = req.user.userId;
-  const isHaveQuota = await quota.checkQuota(userId);
-  const result = await quota.reduceQuota(userId);
-  const { user, token } = result;
-  res.send({ isHaveQuota, user, token });
-  //   try {
-  //     const userId = req.user.userId;
+  try {
+    const userId = req.user.userId;
+    const { image_url } = req.body; 
 
-  //     // Membuat objek FormData untuk mengirim data gambar
-  //     const formData = new FormData();
-  //     formData.append("image", req.body.image);
+    // Check quota scan
+    const isHaveQuota = await quota.checkQuota(userId);
 
-  //     const isHaveQuota = await checkQuota(userId);
+    if (!isHaveQuota) {
+      return res.status(403).send({ error: 'Quota exceeded' });
+    }
 
-  //     // Mengirimkan gambar ke endpoint Flask untuk diproses
-  //     const response = await sendImageToFlaskEndpoint(formData);
+    // Send image_url to function
+    const responseFlask = await sendImageUrlToFlask(image_url);
 
-  //     // Tampilkan pesan respons dari Flask
-  //     console.log("Flask Response:", response);
+    // Reduced quota after scan
+    const result = await quota.reduceQuota(userId);
+    const { user, token } = result;
 
-  //     // Lakukan sesuatu dengan data yang diproses dari Flask, misalnya simpan dalam variabel
-  //     const result = await reduceQuota(userId);
-  //     const { user, token } = result;
-
-  //     res.send({ isHaveQuota, processedData: response, user, token });
-  //   } catch (error) {
-  //     // Tangani kesalahan jika ada
-  //     console.error("Error:", error.message);
-  //     res.status(500).send({ error: "Internal Server Error" });
-  //   }
+    res.send({ isHaveQuota, user, token, modelResponse: responseFlask });
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 };
 
 module.exports = { processData };
